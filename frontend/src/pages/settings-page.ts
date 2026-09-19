cbab966ef7defe8ef50b9b0301c9d1a2eed147da
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2026-09-19 14:28:23 +0100

    Move command launcher from Device to Console

--- frontend/src/pages/settings-page.ts
+++ frontend/src/pages/settings-page.ts
@@ -963,11 +963,6 @@ export class SettingsPage extends LitElement {
                 View Hidden Sensors (${(this._hiddenSensors[this._getCompanionDeviceKey()] || []).length})
               </button>
 
-              <button class="modal-action" @click=${this._openCommandDialogForCompanion}>
-                <span class="modal-action-icon"><svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M20 19V7H4v12h16m0-16a2 2 0 012 2v14a2 2 0 01-2 2H4a2 2 0 01-2-2V5a2 2 0 012-2h16m-7 14v-2h5v2h-5m-3.42-4L5.57 9H8.4l3.3 3.3c.39.39.39 1.03 0 1.42L8.42 17H5.59l4-4z"/></svg></span>
-                Issue Command
-              </button>
-
               <div class="modal-divider"></div>
 
               <button class="modal-action danger" @click=${this._handleRebootFromModal}>
